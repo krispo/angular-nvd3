b97089510a3982593fb99048c44a8f906f137db5
krispo <konstantin.skipor@gmail.com>
2014-05-03 01:24:29 +0600

    update

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var app = angular.module('mainApp', ['ngRoute']).
-    config(['$routeProvider', function($routeProvider) {
+var app = angular.module('mainApp', ['ngRoute'])
+    .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'pages/home.html', controller: 'mainCtrl'});
         $routeProvider.when('/quickstart', {templateUrl: 'pages/quickstart.html', controller: 'mainCtrl'});
         $routeProvider.when('/liveedit', {templateUrl: 'pages/liveedit.html', controller: 'mainCtrl'});
         $routeProvider.otherwise({redirectTo: '/'});
     }])
-
-    /* Controller */
-    .controller('mainCtrl', function(){})
+    .controller('mainCtrl', function($scope, $location){
+        $scope.isActive = function(viewLocation){
+            return viewLocation === $location.path();
+        };
+    })
