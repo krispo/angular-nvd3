6b6f5cec1d850c45817e6a9dfe02887f42e5942e
krispo <konstantin.skipor@gmail.com>
2014-05-05 16:37:28 +0600

    updated with styles, added linechart

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 'use strict';
 
-var app = angular.module('mainApp', ['ngRoute'])
+var app = angular.module('mainApp', ['mainApp.controllers','ngRoute', 'json-tree', 'nvd3'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'pages/home.html', controller: 'mainCtrl'});
         $routeProvider.when('/quickstart', {templateUrl: 'pages/quickstart.html', controller: 'mainCtrl'});
         $routeProvider.when('/liveedit', {templateUrl: 'pages/liveedit.html', controller: 'mainCtrl'});
+
+        $routeProvider.when('/linechart', {templateUrl: 'pages/liveedit.html', controller: 'lineChartCtrl'});
+
         $routeProvider.otherwise({redirectTo: '/'});
     }])
     .controller('mainCtrl', function($scope, $location){
---
+++ js/lineChart.js
@@ -0,0 +1,72 @@
+'use strict';
+
+angular.module('mainApp.controllers', [])
+
+    .controller('lineChartCtrl', function($scope){
+        $scope.options = {
+            chart: {
+                type: 'lineChart',
+                height: 450,
+                width: 700,
+                margin : {
+                    top: 20,
+                    right: 20,
+                    bottom: 60,
+                    left: 40
+                },
+                x: function(d){ return d.x; },
+                y: function(d){ return d.y; },
+                useInteractiveGuideline: true,
+                dispatch: {
+                    stateChange: function(e){ console.log("stateChange"); },
+                    changeState: function(e){ console.log("changeState"); },
+                    tooltipShow: function(e){ console.log("tooltipShow"); },
+                    tooltipHide: function(e){ console.log("tooltipHide"); }
+                },
+                xAxis: {
+                    axisLabel: 'Time (ms)'
+                },
+                yAxis: {
+                    axisLabel: 'Voltage (v)',
+                    tickFormat: function(d){
+                        return d3.format('.02f')(d);
+                    }
+                }
+            }
+        };
+
+        $scope.data = sinAndCos();
+
+        /*Random Data Generator */
+        function sinAndCos() {
+            var sin = [],sin2 = [],
+                cos = [];
+
+            //Data is represented as an array of {x,y} pairs.
+            for (var i = 0; i < 100; i++) {
+                sin.push({x: i, y: Math.sin(i/10)});
+                sin2.push({x: i, y: Math.sin(i/10) *0.25 + 0.5});
+                cos.push({x: i, y: .5 * Math.cos(i/10)});
+            }
+
+            //Line chart data should be sent as an array of series objects.
+            return [
+                {
+                    values: sin,      //values - represents the array of {x,y} data points
+                    key: 'Sine Wave', //key  - the name of the series.
+                    color: '#ff7f0e'  //color - optional: choose your own line color.
+                },
+                {
+                    values: cos,
+                    key: 'Cosine Wave',
+                    color: '#2ca02c'
+                },
+                {
+                    values: sin2,
+                    key: 'Another sine wave',
+                    color: '#7777ff',
+                    area: true      //area - set to true if you want this line to turn into a filled area chart.
+                }
+            ];
+        };
+    })
