f8673b459848a583fae2fd5af72ed5ce8d89c83b
krispo <konstantin.skipor@gmail.com>
2014-05-05 13:02:30 +0600

    added dependencies

---
+++ bower_components/angular-nvd3/Gruntfile.js
@@ -0,0 +1,61 @@
+/* global module */
+
+module.exports = function(grunt){
+
+    'use strict';
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            options: {
+                banner:
+                        '/**************************************************************************\n' +
+                        '* <%= pkg.title || pkg.name %>, ' +
+                        'v<%= pkg.version %>; ' +
+                        '<%= pkg.license %>; ' +
+                        '<%= grunt.template.today("mm/dd/yyyy HH:MM") %>\n' +
+                        '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+                        '**************************************************************************/\n',
+                stripBanners: true
+            },
+
+            dist: {
+                src: ['src/*.js'],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+
+        uglify: {
+            options: {
+                mangle: false
+            },
+            min: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['dist/<%= pkg.name %>.js']
+                }
+            }
+        },
+
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            afterconcat: ['dist/<%= pkg.name %>.js'],
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+        },
+
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['concat', 'jshint', 'uglify']);
+};
---
+++ bower_components/angular-nvd3/README.md
@@ -0,0 +1,1 @@
+# angular-nvd3
