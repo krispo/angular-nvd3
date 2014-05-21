c16dd323bd4f9002a53c9e49e00716b85cd6503d
krispo <konstantin.skipor@gmail.com>
2014-05-21 23:03:29 +0600

    update with global api and events

--- dist/angular-nvd3.js
+++ dist/angular-nvd3.js
@@ -1,5 +1,5 @@
 /**************************************************************************
-* AngularJS-nvD3, v0.0.3; MIT; 05/07/2014 13:22
+* AngularJS-nvD3, v0.0.4; MIT; 05/21/2014 23:02
 * http://krispo.github.io/angular-nvd3
 **************************************************************************/
 (function(){
@@ -12,124 +12,129 @@
             return {
                 restrict: 'AE',
                 scope: {
-                    data: '=',
-                    options: '='
+                    data: '=',      //chart data, [required]
+                    options: '=',   //chart options, according to nvd3 core api, [required]
+                    api: '=?',      //directive global api, [optional]
+                    events: '=?'    //global events that directive would subscribe to, [optional]
                 },
                 link: function(scope, element, attrs){
                     var chart, svg;
 
-                    // Update chart after options have changed
-                    function updateWithOptions(options){
-                        // Clearing
-                        clearElement();
+                    //directive global api
+                    scope.api = {
+                        // Update chart after options have changed
+                        updateWithOptions: function(options){
+                            // Clearing
+                            scope.api.clearElement();
 
-                        // Initialize chart with specific type
-                        chart = nv.models[options.chart.type]();
+                            // Initialize chart with specific type
+                            chart = nv.models[options.chart.type]();
 
-                        angular.forEach(chart, function(value, key){
-                            if (key === 'options');
+                            angular.forEach(chart, function(value, key){
+                                if (key === 'options');
 
-                            else if (key === 'dispatch') {
-                                if (options.chart[key] === undefined) options.chart[key] = {};
-                                configureEvents(chart[key], options.chart[key]);
+                                else if (key === 'dispatch') {
+                                    if (options.chart[key] === undefined) options.chart[key] = {};
+                                    configureEvents(chart[key], options.chart[key]);
+                                }
+
+                                else if ([
+                                    'lines',
+                                    'lines1',
+                                    'lines2',
+                                    'bars', // TODO: Fix bug in nvd3, nv.models.historicalBar - chart.interactive (false -> _)
+                                    'bars1',
+                                    'bars2',
+                                    'stack1',
+                                    'stack2',
+                                    'multibar',
+                                    'discretebar',
+                                    'pie',
+                                    'scatter',
+                                    'bullet',
+                                    'sparkline',
+                                    'legend',
+                                    'distX',
+                                    'distY',
+                                    'xAxis',
+                                    'x2Axis',
+                                    'yAxis',
+                                    'yAxis1',
+                                    'yAxis2',
+                                    'y1Axis',
+                                    'y2Axis',
+                                    'y3Axis',
+                                    'y4Axis',
+                                    'interactiveLayer',
+                                    'controls'
+                                ].indexOf(key) >= 0){
+                                    if (options.chart[key] === undefined) options.chart[key] = {};
+                                    configure(chart[key], options.chart[key], options.chart.type);
+                                }
+
+                                else if (//TODO: need to fix bug in nvd3
+                                    (key ==='clipEdge' && options.chart.type === 'multiBarHorizontalChart')
+                                        || (key === 'clipVoronoi' && options.chart.type === 'historicalBarChart')
+                                        || (key === 'color' && options.chart.type === 'indentedTreeChart')
+                                        || (key === 'defined' && (options.chart.type === 'historicalBarChart' || options.chart.type === 'cumulativeLineChart' || options.chart.type === 'lineWithFisheyeChart'))
+                                        || (key === 'forceX' && (options.chart.type === 'multiBarChart' || options.chart.type === 'discreteBarChart' || options.chart.type === 'multiBarHorizontalChart'))
+                                        || (key === 'interpolate' && options.chart.type === 'historicalBarChart')
+                                        || (key === 'isArea' && options.chart.type === 'historicalBarChart')
+                                        || (key === 'size' && options.chart.type === 'historicalBarChart')
+                                        || (key === 'stacked' && options.chart.type === 'stackedAreaChart')
+                                        || (key === 'values' && options.chart.type === 'pieChart')
+                                        || (key === 'xScale' && options.chart.type === 'scatterChart')
+                                        || (key === 'yScale' && options.chart.type === 'scatterChart')
+                                        || (key === 'x' && (options.chart.type === 'lineWithFocusChart' || options.chart.type === 'multiChart'))
+                                        || (key === 'y' && options.chart.type === 'lineWithFocusChart' || options.chart.type === 'multiChart')
+                                    );
+
+                                else (options.chart[key] === undefined || options.chart[key] === null)
+                                        ? options.chart[key] = value()
+                                        : chart[key](options.chart[key]);
+                            });
+
+                            // Select the current element to add <svg> element and to render the chart in
+                            svg = d3.select(element[0]).append('svg')
+                                .attr('height', options.chart.height)
+                                .attr('width', options.chart.width);
+
+                            // Update with data
+                            scope.api.updateWithData(scope.data);
+
+                            // Configure wrappers
+                            configureWrapper('title');
+                            configureWrapper('subtitle');
+                            configureWrapper('caption');
+
+                            // Configure styles
+                            configureStyles();
+
+                            nv.addGraph(function() {
+                                // Update the chart when window resizes
+                                nv.utils.windowResize(function() { chart.update(); });
+                                return chart;
+                            });
+                        },
+
+                        // Update chart after data have changed
+                        updateWithData: function (data){
+                            if (data && svg) {
+                                scope.options.chart['transitionDuration'] = +scope.options.chart['transitionDuration'] || 250;
+                                svg.datum(data)
+                                    .transition().duration(scope.options.chart['transitionDuration'])
+                                    .call(chart);
                             }
+                        },
 
-                            else if ([
-                                'lines',
-                                'lines1',
-                                'lines2',
-                                'bars', // TODO: Fix bug in nvd3, nv.models.historicalBar - chart.interactive (false -> _)
-                                'bars1',
-                                'bars2',
-                                'stack1',
-                                'stack2',
-                                'multibar',
-                                'discretebar',
-                                'pie',
-                                'scatter',
-                                'bullet',
-                                'sparkline',
-                                'legend',
-                                'distX',
-                                'distY',
-                                'xAxis',
-                                'x2Axis',
-                                'yAxis',
-                                'yAxis1',
-                                'yAxis2',
-                                'y1Axis',
-                                'y2Axis',
-                                'y3Axis',
-                                'y4Axis',
-                                'interactiveLayer',
-                                'controls'
-                            ].indexOf(key) >= 0){
-                                if (options.chart[key] === undefined) options.chart[key] = {};
-                                configure(chart[key], options.chart[key], options.chart.type);
-                            }
-
-                            else if (//TODO: need to fix bug in nvd3
-                                (key ==='clipEdge' && options.chart.type === 'multiBarHorizontalChart')
-                                    || (key === 'clipVoronoi' && options.chart.type === 'historicalBarChart')
-                                    || (key === 'color' && options.chart.type === 'indentedTreeChart')
-                                    || (key === 'defined' && (options.chart.type === 'historicalBarChart' || options.chart.type === 'cumulativeLineChart' || options.chart.type === 'lineWithFisheyeChart'))
-                                    || (key === 'forceX' && (options.chart.type === 'multiBarChart' || options.chart.type === 'discreteBarChart' || options.chart.type === 'multiBarHorizontalChart'))
-                                    || (key === 'interpolate' && options.chart.type === 'historicalBarChart')
-                                    || (key === 'isArea' && options.chart.type === 'historicalBarChart')
-                                    || (key === 'size' && options.chart.type === 'historicalBarChart')
-                                    || (key === 'stacked' && options.chart.type === 'stackedAreaChart')
-                                    || (key === 'values' && options.chart.type === 'pieChart')
-                                    || (key === 'xScale' && options.chart.type === 'scatterChart')
-                                    || (key === 'yScale' && options.chart.type === 'scatterChart')
-                                    || (key === 'x' && (options.chart.type === 'lineWithFocusChart' || options.chart.type === 'multiChart'))
-                                    || (key === 'y' && options.chart.type === 'lineWithFocusChart' || options.chart.type === 'multiChart')
-                                );
-
-                            else (options.chart[key] === undefined || options.chart[key] === null)
-                                    ? options.chart[key] = value()
-                                    : chart[key](options.chart[key]);
-                        });
-
-                        // Select the current element to add <svg> element and to render the chart in
-                        svg = d3.select(element[0]).append('svg')
-                            .attr('height', options.chart.height)
-                            .attr('width', options.chart.width);
-
-                        // Update with data
-                        updateWithData(scope.data);
-
-                        // Configure wrappers
-                        configureWrapper('title');
-                        configureWrapper('subtitle');
-                        configureWrapper('caption');
-
-                        // Configure styles
-                        configureStyles();
-
-                        nv.addGraph(function() {
-                            // Update the chart when window resizes
-                            nv.utils.windowResize(function() { chart.update(); });
-                            return chart;
-                        });
-                    }
-
-                    // Update chart after data have changed
-                    function updateWithData(data){
-                        if (data && svg) {
-                            scope.options.chart['transitionDuration'] = +scope.options.chart['transitionDuration'] || 250;
-                            svg.datum(data)
-                                .transition().duration(scope.options.chart['transitionDuration'])
-                                .call(chart);
+                        // Fully clear directive element
+                        clearElement: function (){
+                            element.find('.title').remove();
+                            element.find('.subtitle').remove();
+                            element.find('.caption').remove();
+                            element.empty();
                         }
-                    }
-
-                    // Fully clear directive element
-                    function clearElement(){
-                        element.find('.title').remove();
-                        element.find('.subtitle').remove();
-                        element.find('.caption').remove();
-                        element.empty();
-                    }
+                    };
 
                     // Configure the chart model with the passed options
                     function configure(chart, options, chartType){
@@ -252,8 +257,15 @@
                     }
 
                     // Watching on options and data changing
-                    scope.$watch('options', function(options){ updateWithOptions(options); }, true);
-                    scope.$watch('data', function(data){ updateWithData(data); }, true);
+                    scope.$watch('options', function(options){ scope.api.updateWithOptions(options); }, true);
+                    scope.$watch('data', function(data){ scope.api.updateWithData(data); }, true);
+
+                    //subscribe on global events
+                    angular.forEach(scope.events, function(eventHandler, event){
+                        scope.$on(event, function(){
+                            return eventHandler(scope);
+                        });
+                    });
                 }
             };
         }]);
