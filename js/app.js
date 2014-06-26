bac671cb346204154c4cd5da20d1721512458f40
krispo <konstantin.skipor@gmail.com>
2014-06-26 22:48:48 +0600

    updated with links

--- js/app.js
+++ js/app.js
@@ -130,24 +130,24 @@ var app = angular.module('mainApp', ['mainApp.controllers','ngRoute', 'json-tree
  */
     .constant('CHARTS', {
         lineChart: { path: '/lineChart', title: 'Line Chart', plunker: 'http://plnkr.co/edit/lBKFld?p=preview' },
-        cumulativeLineChart: { path: '/cumulativeLineChart', title: 'Cumulative Line Chart' },
-        stackedAreaChart: { path: '/stackedAreaChart', title: 'Stacked Area Chart' },
-        multiBarChart: { path: '/multiBarChart', title: 'MultiBar Chart' },
-        discreteBarChart: { path: '/discreteBarChart', title: 'DiscreteBar Chart' },
-        historicalBarChart: { path: '/historicalBarChart', title: 'HistoricalBar Chart' },
-        multiBarHorizontalChart: { path: '/multiBarHorizontalChart', title: 'MultiBar Horizontal Chart' },
-        pieChart: { path: '/pieChart', title: 'Pie Chart' },
-        scatterChart: { path: '/scatterChart', title: 'Scatter Chart' },
-        lineWithFocusChart: { path: '/lineWithFocusChart', title: 'Line with Focus Chart' },
-        linePlusBarChart: { path: '/linePlusBarChart', title: 'Line + Bar Chart' },
-        scatterPlusLineChart: { path: '/scatterPlusLineChart', title: 'Scatter + Line Chart' },
-        linePlusBarWithFocusChart: { path: '/linePlusBarWithFocusChart', title: 'Line + Bar with Focus Chart' },
-        donutChart: { path: '/donutChart', title: 'Donut Chart' },
-        bulletChart: { path: '/bulletChart', title: 'Bullet Chart' },
-        sparklinePlus: { path: '/sparklinePlus', title: 'SparkLine Chart' },
-        parallelCoordinates: { path: '/parallelCoordinates', title: 'Parallel Coordinates' },
-        multiChart: { path: '/multiChart', title: 'Multi Chart' },
-        lineWithFisheyeChart: { path: '/lineWithFisheyeChart', title: 'Line with Fisheye Chart' }
+        cumulativeLineChart: { path: '/cumulativeLineChart', title: 'Cumulative Line Chart', plunker: 'http://plnkr.co/edit/iIxJT3?p=preview' },
+        stackedAreaChart: { path: '/stackedAreaChart', title: 'Stacked Area Chart', plunker: 'http://plnkr.co/edit/CIGW0o?p=preview' },
+        multiBarChart: { path: '/multiBarChart', title: 'MultiBar Chart', plunker: 'http://plnkr.co/edit/zGc5Wp?p=preview' },
+        discreteBarChart: { path: '/discreteBarChart', title: 'DiscreteBar Chart', plunker: 'http://plnkr.co/edit/6t5bky?p=preview' },
+        historicalBarChart: { path: '/historicalBarChart', title: 'HistoricalBar Chart', plunker: 'http://plnkr.co/edit/U1wWbe?p=preview' },
+        multiBarHorizontalChart: { path: '/multiBarHorizontalChart', title: 'MultiBar Horizontal Chart', plunker: 'http://plnkr.co/edit/UZGxhp?p=preview' },
+        pieChart: { path: '/pieChart', title: 'Pie Chart', plunker: 'http://plnkr.co/edit/vtKWU0?p=preview' },
+        scatterChart: { path: '/scatterChart', title: 'Scatter Chart', plunker: 'http://plnkr.co/edit/6hjwSA?p=preview' },
+        lineWithFocusChart: { path: '/lineWithFocusChart', title: 'Line with Focus Chart', plunker: 'http://plnkr.co/edit/sWONMb?p=preview' },
+        linePlusBarChart: { path: '/linePlusBarChart', title: 'Line + Bar Chart', plunker: 'http://plnkr.co/edit/UASCUL?p=preview' },
+        scatterPlusLineChart: { path: '/scatterPlusLineChart', title: 'Scatter + Line Chart', plunker: 'http://plnkr.co/edit/2MjNgj?p=preview' },
+        linePlusBarWithFocusChart: { path: '/linePlusBarWithFocusChart', title: 'Line + Bar with Focus Chart', plunker: 'http://plnkr.co/edit/f0QET0?p=preview' },
+        donutChart: { path: '/donutChart', title: 'Donut Chart', plunker: 'http://plnkr.co/edit/jOoJik?p=preview' },
+        bulletChart: { path: '/bulletChart', title: 'Bullet Chart', plunker: 'http://plnkr.co/edit/Mb1cWN?p=preview' },
+        sparklinePlus: { path: '/sparklinePlus', title: 'SparkLine Chart', plunker: 'http://plnkr.co/edit/9ARpl6?p=preview' },
+        parallelCoordinates: { path: '/parallelCoordinates', title: 'Parallel Coordinates', plunker: 'http://plnkr.co/edit/rCQhcL?p=preview' },
+        multiChart: { path: '/multiChart', title: 'Multi Chart', plunker: 'http://plnkr.co/edit/zsNxBJ?p=preview' },
+        lineWithFisheyeChart: { path: '/lineWithFisheyeChart', title: 'Line with Fisheye Chart', plunker: 'http://plnkr.co/edit/YFruJE?p=preview' }
     })
 
     .constant('CONSTANTS', {
