61c2a8cffcbd55137cfc12f97c20019f9e623498
krispo <konstantin.skipor@gmail.com>
2015-09-07 09:49:19 +0300

    added sunburst chart

--- js/app.js
+++ js/app.js
@@ -150,6 +150,7 @@ var app = angular.module('mainApp', ['mainApp.controllers','ngRoute', 'json-tree
         multiChart: { path: '/multiChart', title: 'Multi Chart', plunker: 'http://plnkr.co/edit/zsNxBJ?p=preview' },
         //lineWithFisheyeChart: { path: '/lineWithFisheyeChart', title: 'Line with Fisheye Chart', plunker: 'http://plnkr.co/edit/YFruJE?p=preview' }
         candlestickBarChart: { path: '/candlestickBarChart', title: 'Candlestick Chart', plunker: '' },
+        sunburstChart: { path: '/sunburstChart', title: 'Sunburst Chart', plunker: '' },
     })
 
     .constant('CONSTANTS', {
