86e8745f9d497e9966ae24690630a62a704cd674
krispo <konstantin.skipor@gmail.com>
2015-09-08 21:46:22 +0300

    changed version

--- js/app.js
+++ js/app.js
@@ -156,7 +156,7 @@ var app = angular.module('mainApp', ['mainApp.controllers','ngRoute', 'json-tree
     })
 
     .constant('CONSTANTS', {
-        version: '1.0.0-rc.2',
+        version: '1.0.1',
         productionHostname: 'krispo.github.io',
         otherCharts: [
             { name: "Real-time chart updating", url: "http://plnkr.co/edit/yKFROu?p=preview" },
